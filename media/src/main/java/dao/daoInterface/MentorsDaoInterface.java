183ad4e8591fe438debc4b2ed13dae1dc404e1dc
Adelaiten <karoltrzaska19@gmail.com>
2018-12-12 12:04:30 +0100

    refactor mentordaointerface

--- media/src/main/java/dao/daoInterface/MentorsDaoInterface.java
+++ media/src/main/java/dao/daoInterface/MentorsDaoInterface.java
@@ -8,7 +8,7 @@ import java.util.List;
 public interface MentorsDaoInterface {
 
     List<Mentor> getAllMentors() throws SQLException;
-    Mentor getMentorById() throws SQLException;
+    Mentor getMentorById(int id) throws SQLException;
     List<Mentor> getMentorsFirstAndLastName() throws SQLException;
     List<Mentor> getMentorsNickNamesByCity(String city) throws SQLException;
 
