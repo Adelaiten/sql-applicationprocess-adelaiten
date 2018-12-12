6fcd2c86b6e68748773d725fbd397c300480d099
Adelaiten <karoltrzaska19@gmail.com>
2018-12-12 09:55:39 +0100

    select first and last name as one full name with where query

--- media/src/main/queries/MentorsSelectQuery.sql
+++ media/src/main/queries/MentorsSelectQuery.sql
@@ -1,2 +1,3 @@
 SELECT first_name, last_name FROM mentors;
 SELECT nick_name FROM mentors WHERE city = '''Miskolc''';
+SELECT concat(first_name, ' ', last_name) AS full_name, phone_number FROM applicants WHERE first_name = '''Carol''';
