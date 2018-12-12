a20ac2500e6d44b60e0f5c86c11dc171f3723ed5
Adelaiten <karoltrzaska19@gmail.com>
2018-12-12 10:43:13 +0100

    mentor model

---
+++ media/src/main/java/models/Mentor.java
@@ -0,0 +1,76 @@
+package models;
+
+public class Mentor {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String nickName;
+    private String phoneNumber;
+    private String email;
+    private String city;
+    private int favouriteNumber;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public int getFavouriteNumber() {
+        return favouriteNumber;
+    }
+
+    public void setFavouriteNumber(int favouriteNumber) {
+        this.favouriteNumber = favouriteNumber;
+    }
+}
