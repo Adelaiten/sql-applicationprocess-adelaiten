26ee9ca32b3b2f6f5b89dfa883ecde39e87fe3bc
Adelaiten <karoltrzaska19@gmail.com>
2018-12-12 10:03:03 +0100

    insert into query

--- media/src/main/queries/MentorsSelectQuery.sql
+++ media/src/main/queries/MentorsSelectQuery.sql
@@ -2,3 +2,4 @@ SELECT first_name, last_name FROM mentors;
 SELECT nick_name FROM mentors WHERE city = '''Miskolc''';
 SELECT concat(first_name, ' ', last_name) AS full_name, phone_number FROM applicants WHERE first_name = '''Carol''';
 SELECT concat(first_name, ' ', last_name) AS full_name, phone_number FROM applicants WHERE email like '''%@adipiscingenimmi.edu''';
+INSERT INTO applicants(first_name, last_name, phone_number, email, application_code) VALUES('Markus', 'Schaffarzyk', '003620/725-2666', 'djnovus@groovecoverage.com ', 54823);
