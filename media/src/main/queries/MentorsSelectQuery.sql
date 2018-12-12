4d0d84fa55f72a9de2179fb952464047a0c6ad8b
Adelaiten <karoltrzaska19@gmail.com>
2018-12-12 10:04:26 +0100

    select with where application_code clause query

--- media/src/main/queries/MentorsSelectQuery.sql
+++ media/src/main/queries/MentorsSelectQuery.sql
@@ -3,3 +3,4 @@ SELECT nick_name FROM mentors WHERE city = '''Miskolc''';
 SELECT concat(first_name, ' ', last_name) AS full_name, phone_number FROM applicants WHERE first_name = '''Carol''';
 SELECT concat(first_name, ' ', last_name) AS full_name, phone_number FROM applicants WHERE email like '''%@adipiscingenimmi.edu''';
 INSERT INTO applicants(first_name, last_name, phone_number, email, application_code) VALUES('Markus', 'Schaffarzyk', '003620/725-2666', 'djnovus@groovecoverage.com ', 54823);
+SELECT * FROM applicants WHERE application_code = 54823;
