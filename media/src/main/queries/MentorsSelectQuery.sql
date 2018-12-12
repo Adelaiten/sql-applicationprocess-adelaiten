bdeef81da07dbd68853a180673b2cc6f2befef99
Adelaiten <karoltrzaska19@gmail.com>
2018-12-12 10:25:51 +0100

    Update jemima foreman phone number query

--- media/src/main/queries/MentorsSelectQuery.sql
+++ media/src/main/queries/MentorsSelectQuery.sql
@@ -4,3 +4,4 @@ SELECT concat(first_name, ' ', last_name) AS full_name, phone_number FROM applic
 SELECT concat(first_name, ' ', last_name) AS full_name, phone_number FROM applicants WHERE email like '''%@adipiscingenimmi.edu''';
 INSERT INTO applicants(first_name, last_name, phone_number, email, application_code) VALUES('Markus', 'Schaffarzyk', '003620/725-2666', 'djnovus@groovecoverage.com ', 54823);
 SELECT * FROM applicants WHERE application_code = 54823;
+UPDATE applicants SET phone_number = '003670/223-7459' WHERE first_name = '''Jemima''' AND last_name = '''Foreman''';
