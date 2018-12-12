d16e3e43b2f438e1308f7f36be038bb6f87059b2
Adelaiten <karoltrzaska19@gmail.com>
2018-12-12 12:00:01 +0100

    get all mentors and all informations method

--- media/src/main/java/dao/MentorsDAO.java
+++ media/src/main/java/dao/MentorsDAO.java
@@ -39,7 +39,23 @@ public class MentorsDAO implements MentorsDaoInterface {
     }
 
     public List<Mentor> getAllMentors() throws SQLException {
-        return null;
+        String query = "SELECT * FROM mentors;";
+        PreparedStatement preparedStatement = connection.prepareStatement(query);
+        List<Mentor> mentorsList = new ArrayList<Mentor>();
+        ResultSet resultSet = preparedStatement.executeQuery();
+        while(resultSet.next()){
+            Mentor mentor = new Mentor();
+            mentor.setId(resultSet.getInt("id"));
+            mentor.setFirstName(resultSet.getString("first_name"));
+            mentor.setLastName(resultSet.getString("last_name"));
+            mentor.setNickName(resultSet.getString("nick_name"));
+            mentor.setPhoneNumber(resultSet.getString("phone_number"));
+            mentor.setEmail(resultSet.getString("email"));
+            mentor.setCity(resultSet.getString("city"));
+            mentor.setFavouriteNumber(resultSet.getInt("favourite_number"));
+            mentorsList.add(mentor);
+        }
+        return mentorsList;
     }
 
     public Mentor getMentorById() throws SQLException {
