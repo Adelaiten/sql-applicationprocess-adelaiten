7216d7d1298ed1f5ecd266e766ce2b885410686c
Adelaiten <karoltrzaska19@gmail.com>
2018-12-12 11:48:47 +0100

    read all applicants method

--- media/src/main/java/dao/ApplicantsDAO.java
+++ media/src/main/java/dao/ApplicantsDAO.java
@@ -7,11 +7,31 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ApplicantsDAO implements ApplicantsDaoInterface {
     Connection connection;
     public ApplicantsDAO(Connection connection) {
         this.connection = connection;
+    }
+
+    public List<Applicant> getAllApplicants() throws SQLException {
+        String query = "SELECT * FROM applicants";
+        PreparedStatement preparedStatement = connection.prepareStatement(query);
+        List<Applicant> applicantsList = new ArrayList<Applicant>();
+        ResultSet resultSet = preparedStatement.executeQuery();
+        while(resultSet.next()){
+            Applicant applicant = new Applicant();
+            applicant.setId(resultSet.getInt("id"));
+            applicant.setFirstName(resultSet.getString("first_name"));
+            applicant.setLastName(resultSet.getString("last_name"));
+            applicant.setPhoneNumber(resultSet.getString("phone_number"));
+            applicant.setEmail(resultSet.getString("email"));
+            applicant.setApplicationCode(resultSet.getInt("application_code"));
+            applicantsList.add(applicant);
+        }
+        return applicantsList;
     }
     public Applicant getApplicantByFirstName(String firstName) throws SQLException {
         String query = "SELECT * FROM applicants WHERE first_name=?;";
--- media/src/main/java/dao/daoInterface/ApplicantsDaoInterface.java
+++ media/src/main/java/dao/daoInterface/ApplicantsDaoInterface.java
@@ -3,8 +3,10 @@ package dao.daoInterface;
 import models.Applicant;
 
 import java.sql.SQLException;
+import java.util.List;
 
 public interface ApplicantsDaoInterface {
+    List<Applicant> getAllApplicants() throws SQLException;
     Applicant getApplicantByFirstName(String firstName) throws SQLException;
     Applicant getApplicantByEmail(String email) throws SQLException;
     void addApplicant(Applicant applicant) throws SQLException;
