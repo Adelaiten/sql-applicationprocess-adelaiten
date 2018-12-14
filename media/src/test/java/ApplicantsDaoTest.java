c8bd3e760b6890e251f76a8df77ce00343722e6c
Adelaiten <karoltrzaska19@gmail.com>
2018-12-14 23:14:22 +0100

    getApplicantById method if returning applicant test

--- media/src/test/java/ApplicantsDaoTest.java
+++ media/src/test/java/ApplicantsDaoTest.java
@@ -2,6 +2,7 @@
 import dao.ApplicantsDAO;
 import dao.daoInterface.ApplicantsDaoInterface;
 import models.Applicant;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
@@ -17,6 +18,7 @@ import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.when;
 
 public class ApplicantsDaoTest {
+
 
     @Test
     public void testGetAllAplicantsFilled() throws SQLException {
@@ -34,5 +36,23 @@ public class ApplicantsDaoTest {
         when(resultSet.getString(Mockito.anyString())).thenReturn("karoltrzaska19@gmail.com");
         when(resultSet.getInt(Mockito.anyString())).thenReturn(1000);
         assertTrue(applicantsDao.getAllApplicants().get(0) != null);
+    }
+
+    @Test
+    public void testGetApplicantByIdReturnApplicant() throws SQLException {
+        Connection connection = mock(Connection.class);
+        ApplicantsDaoInterface applicantsDao = new ApplicantsDAO(connection);
+        ResultSet resultSet = mock(ResultSet.class);
+        PreparedStatement preparedStatement = mock(PreparedStatement.class);
+        when(connection.prepareStatement(Mockito.anyString())).thenReturn(preparedStatement);
+        when(preparedStatement.executeQuery()).thenReturn(resultSet);
+        when(resultSet.next()).thenReturn(true).thenReturn(false);
+        when(resultSet.getInt(Mockito.anyString())).thenReturn(1);
+        when(resultSet.getString(Mockito.anyString())).thenReturn("Karol");
+        when(resultSet.getString(Mockito.anyString())).thenReturn("Trzaska");
+        when(resultSet.getString(Mockito.anyString())).thenReturn("2093821");
+        when(resultSet.getString(Mockito.anyString())).thenReturn("karoltrzaska19@gmail.com");
+        when(resultSet.getInt(Mockito.anyString())).thenReturn(1000);
+        assertTrue(applicantsDao.getApplicantById(2) != null);
     }
 }
